f74f841546ccf9ee8e5bd4e49d1990db830a8fc2
boborbt <boborbt@gmail.com>
2019-06-05 15:42:56 +0200

    Added alternating least squares algorithm

--- src/TLLR.jl
+++ src/TLLR.jl
@@ -4,7 +4,7 @@ using Convex
 using ECOS
 
 import Base.size
-export fit, predict
+export fit, fit_iterative, predict
 using LinearAlgebra
 
 """
@@ -85,6 +85,85 @@ function fit(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}; verbose=
   (opt, a, b, t, P)
 end
 
+
+"""
+    fit_iterative(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}; beta=randomvalues)
+
+Fits a block least squares model by alternating the optimization of the α and β variables.
+
+# Arguments
+
+* `X`: \$N × M\$ matrix describing the examples
+* `y`: \$N\$ vector with the output values for each example
+* `P`: \$M × K\$ matrix specifying how to partition the \$M\$ attributes into
+    \$K\$ subsets. \$P_{m,k}\$ should be 1 if attribute number \$m\$ belongs to
+    partition \$k\$.
+* `verbose`: if true (or 1) the output of solver will be shown
+* `η`: regularization factor, higher values implies more regularized solutions
+
+# Result
+
+A tuple of the form: `(opt, a, b, t, P)`
+
+* `opt`: optimal value of the objective function (loss + regularization)
+* `a`: values of the α variables at the optimal point
+* `b`: values of the β variables at the optimal point
+* `t`: the intercept at the optimal point
+* `P`: the partition matrix (copied from the input)
+
+The output model predicts points using the formula: f(X) = \$X * (P .* a) * b + t\$.
+
+"""
+function fit_iterative(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}; verbose=0, η=1)
+
+  α = Variable(M, Positive())
+  β = Variable(K)
+  t = Variable()
+
+  loss = sumsquares(X * (P .* (α * ones(1,K))) * β + t - y)
+  regularization = η * norm(α,2)
+  p = minimize(loss + regularization)
+
+  for i in 0:100
+    free!(β)
+    fix!(α)
+    Convex.solve!(p, ECOSSolver(verbose=verbose))
+    free!(α)
+    fix!(β)
+    Convex.solve!(p, ECOSSolver(verbose=verbose))
+
+    @info optval = p.optval
+  end
+
+  # # row normalization
+  # M,K = size(P)
+
+  # results = []
+
+  # for b in 0:(2^K-1)
+  #   α = Variable(M, Positive())
+  #   t = Variable()
+  #   β = indextobeta(b,K)
+
+  #   loss = sumsquares(X * (P .* (α * ones(1,K))) * β + t - y)
+  #   regularization = η * norm(α,2)
+  #   p = minimize(loss + regularization)
+  #   Convex.solve!(p, ECOSSolver(verbose=verbose))
+
+  #   @debug "iteration" b "optval:" p.optval
+  #   push!(results,(p.optval, α.value, β, t.value, P))
+  # end
+
+  # optindex = argmin((z -> z[1]).(results))
+  # opt,a,b,t,_ = results[optindex]
+
+
+  # A = sum(P .* a, dims=1)
+  # a = sum((P .* a) ./ A, dims=2)
+  # b = b .* A'
+
+  (p.optval, α.values, β.values, t.value, P)
+end
 
 """
   predict(model::Tuple, X::Array{Float64,2})
