17fd21654853e5064d4e00e67921cd448185abfa
Roberto Esposito <roberto.esposito@unito.it>
2021-12-04 17:39:45 +0100

    Fixed compilation problem for last commit

--- src/PartitionedLSBnB.jl
+++ src/PartitionedLSBnB.jl
@@ -1,11 +1,11 @@
 struct BnB end         # Branch and Bound approach
 
-function fit(::Type{BnB}, X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}; 
-    η = 1.0, get_solver = get_ECOSSolver, nnlsalg=:pivot)
+function fit(::Type{BnB}, X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2};
+    η = 1.0, get_solver = get_ECOSSolver, nnlsalg = :pivot)
     Xo, Po = homogeneousCoords(X, P)
     Σ::Array{Int,1} = []
 
-    opt, α, nopen = fit_BnB(Xo, y, Po, Inf, Σ, get_solver = get_solver, nnlsalg=nnlsalg)
+    opt, α, nopen = fit_BnB(Xo, y, Po, Inf, Σ, get_solver = get_solver, nnlsalg = nnlsalg)
     β = sum(Po .* α, dims = 1)
     α = sum(Po .* α ./ β, dims = 2)
 
@@ -13,14 +13,14 @@ function fit(::Type{BnB}, X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int
 end
 
 function sum_max_0_αi_αj(P::Array{Int,2}, α::Array{Float64,1})
-    K = size(P,2)   # number of partitions
+    K = size(P, 2)   # number of partitions
     result = zeros(K)
 
     # forall k, sets result[k] = sum(max(0, -αi*αj)) forall i,j in partition k
-    for k in 1:K
+    for k = 1:K
         is = findall(!=(0), P[:, k])   # list of indices of partition k
-        for i in 1:length(is)
-            for j in (i+1):length(is)
+        for i = 1:length(is)
+            for j = (i+1):length(is)
                 result[k] += max(0, -α[is[i]]α[is[j]])
             end
         end
@@ -29,12 +29,12 @@ function sum_max_0_αi_αj(P::Array{Int,2}, α::Array{Float64,1})
     return result
 end
 
-function saveproblem(XX,y)
-    using CSV
-    using DataFrames
+using CSV
+using DataFrames
+function saveproblem(XX, y)
     df = DataFrame(XX, :auto)
     df.y = y
-    
+
     @warn "Writing temporary csv file -- this is a debug feature, should not be present in production"
     CSV.write("nnls_problem.csv", df)
 end
@@ -52,8 +52,8 @@ function lower_bound(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}, 
     XX = [Xp Xm]
 
     @debug "Launching nonneg_lsq"
-    saveproblem(XX,y)
-    αα = nonneg_lsq(XX, y, alg=nnlsalg)
+    saveproblem(XX, y)
+    αα = nonneg_lsq(XX, y, alg = nnlsalg)
     @debug "nonneg_lsq terminated"
     αp = αα[1:M]
     αn = αα[M+1:end]
@@ -65,10 +65,10 @@ function lower_bound(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}, 
     return norm(XX * αα - y), α
 end
 
-function fit_BnB(X::Array{Float64,2}, y::Array{Float64,1}, P::Matrix{Int}, μ::Float64, Σ::Array{Int,1}; 
-                 get_solver = get_ECOSSolver, 
-                 depth = 0,
-                 nnlsalg = :pivot)::Tuple{Float64, Vector{Float64}, Int}
+function fit_BnB(X::Array{Float64,2}, y::Array{Float64,1}, P::Matrix{Int}, μ::Float64, Σ::Array{Int,1};
+    get_solver = get_ECOSSolver,
+    depth = 0,
+    nnlsalg = :pivot)::Tuple{Float64,Vector{Float64},Int}
     @debug "BnB new node"
 
     lb, α = lower_bound(X, y, P, Σ, get_solver, nnlsalg)
@@ -84,9 +84,9 @@ function fit_BnB(X::Array{Float64,2}, y::Array{Float64,1}, P::Matrix{Int}, μ::F
     if all(==(0), ν)
         # optimal solution found
         p★ = norm(X * α - y)
-        
+
         @debug "Optimal solution for this node $p★"
-        return (p★,α,1)
+        return (p★, α, 1)
     end
 
     k = argmax(ν)
@@ -100,7 +100,7 @@ function fit_BnB(X::Array{Float64,2}, y::Array{Float64,1}, P::Matrix{Int}, μ::F
 
     i = argmin([μ, μp, μm])
 
-    res = ([μ, μp, μm][i], [α, αp, αm][i], (nopenp+nopenm+1))
+    res = ([μ, μp, μm][i], [α, αp, αm][i], (nopenp + nopenm + 1))
 
     @debug "Best upperbound for this node: $(res[1]) num open nodes so far: $(res[3])"
     return res
