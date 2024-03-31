568905d5044283456b1afc7e129bef23bfb12347
Roberto Esposito <roberto.esposito@unito.it>
2024-03-31 23:04:29 +0200

    Fixing documentation

--- docs/jldocs/make.jl
+++ docs/jldocs/make.jl
@@ -1,15 +1,14 @@
-using Pkg;
-Pkg.activate("PartitionedLSenv", shared=true)
-push!(LOAD_PATH,"../src/")
-
 using PartitionedLS
 using Documenter
-makedocs(
-         sitename = "PartitionedLS.jl",
-         modules  = [PartitionedLS],
-         pages=[
-                "Home" => "index.md"
-               ])
-deploydocs(;
-    repo="github.com/boborbt/PartitionedLS.jl",
-)
+
+makedocs(sitename="My Documentation")
+
+# makedocs(
+#          sitename = "PartitionedLS.jl",
+#          modules  = [PartitionedLS],
+#          pages = [
+#                 "Home" => "index.md"
+#                ])
+# deploydocs(;
+#     repo="github.com/boborbt/PartitionedLS.jl",
+# )
